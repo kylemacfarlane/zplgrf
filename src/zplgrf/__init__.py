8e50ca103dd32065bedc71e79f3478e739777985
Kyle MacFarlane <kyle@deletethetrees.com>
2018-01-28 04:26:41 +0000

    Add media tracking option

--- src/zplgrf/__init__.py
+++ src/zplgrf/__init__.py
@@ -324,7 +324,7 @@ class GRF(object):
 
     def to_zpl(
         self, quantity=1, pause_and_cut=0, override_pause=False,
-        print_mode='C', print_orientation='N', **kwargs
+        print_mode='C', print_orientation='N', media_tracking='Y', **kwargs
     ):
         """
         The most basic ZPL to print the GRF. Since ZPL printers are stateful
@@ -335,6 +335,7 @@ class GRF(object):
             '^XA',  # Start Label Format
             '^MM%s,Y' % print_mode,
             '^PO%s' % print_orientation,
+            '^MN%s' % media_tracking,
             '^FO0,0',  # Field Origin to 0,0
             '^XGR:%s.GRF,1,1' % self.filename,  # Draw image
             '^FS',  # Field Separator
